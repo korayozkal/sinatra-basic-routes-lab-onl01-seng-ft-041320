051d377b7fc3a0b0257d5dde79606466689ef5eb
Koray Ozkal <koryozkal@gmail.com>
2020-06-04 03:55:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,14 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
 
 get '/name' do
-"My name is __"
+""My name is ""
 end
 
-get '/medicines' do
-  @medicines = Medicine.all
- 
-  erb :'medicines/index.html.erb'
 end
